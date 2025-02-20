4a079b230ea94589d3adbbff3a52257c07fb03cf
emanueleivn <e.iovane2@studenti.unisa.it>
2025-02-20 18:24:07 +0100

    Aggiornamento caricamento in locale

--- app/src/main/java/com/matthew/sportiliapp/ContentScreen.kt
+++ app/src/main/java/com/matthew/sportiliapp/ContentScreen.kt
@@ -1,16 +1,6 @@
 package com.matthew.sportiliapp
 
-import android.os.Build
 import android.util.Log
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -19,24 +9,14 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.matthew.sportiliapp.admin.AdminHomeScreen
-import com.matthew.sportiliapp.admin.UtenteNavHost
-import com.matthew.sportiliapp.admin.UtenteScreen
-import com.matthew.sportiliapp.model.Esercizio
-import com.matthew.sportiliapp.model.Giorno
-import com.matthew.sportiliapp.model.Utente
 import com.matthew.sportiliapp.scheda.EsercizioScreen
 import com.matthew.sportiliapp.scheda.GiornoScreen
 import com.matthew.sportiliapp.scheda.SchedaScreen
-
 
 @Composable
 fun ContentScreen(navController: NavHostController) {
@@ -65,11 +45,18 @@ fun ContentScreen(navController: NavHostController) {
                             indicatorColor = MaterialTheme.colorScheme.primary,
                         ),
                         onClick = {
-                            navController2.navigate(item.route) {
-                                // Prevents building a large back stack
-                                popUpTo(navController2.graph.startDestinationId) { saveState = true }
-                                restoreState = true
-                                launchSingleTop = true
+                            if (item.route == "scheda") {
+                                navController2.navigate("scheda") {
+                                    popUpTo(navController2.graph.startDestinationId) { inclusive = true }
+                                    launchSingleTop = true
+                                    restoreState = true
+                                }
+                            } else {
+                                navController2.navigate(item.route) {
+                                    popUpTo(navController2.graph.startDestinationId) { saveState = true }
+                                    restoreState = true
+                                    launchSingleTop = true
+                                }
                             }
                         }
                     )
@@ -88,26 +75,14 @@ fun ContentScreen(navController: NavHostController) {
                 composable("scheda") {
                     SchedaScreen(navController = navController2)
                 }
-                composable(
-                    "impostazioni",
-//                    enterTransition = { slideInHorizontally(initialOffsetX = { it }) + fadeIn() },
-//                    exitTransition = { slideOutHorizontally(targetOffsetX = { it }) + fadeOut() }
-                ) {
-                    ImpostazioniScreen(navController)
+                composable("impostazioni") {
+                    ImpostazioniScreen(navController2)
                 }
-                composable(
-                    "giorno/{giornoId}",
-                    enterTransition = { slideInHorizontally(initialOffsetX = { it }) + fadeIn() },
-                    exitTransition = { slideOutHorizontally(targetOffsetX = { it }) + fadeOut() }
-                ) { backStackEntry ->
+                composable("giorno/{giornoId}") { backStackEntry ->
                     val giornoId = backStackEntry.arguments?.getString("giornoId") ?: return@composable
                     GiornoScreen(navController = navController2, giornoId = giornoId)
                 }
-                composable(
-                    "esercizio/{giornoId}/{gruppoMuscolareId}/{esercizioId}",
-                    enterTransition = { slideInHorizontally(initialOffsetX = { it }) + fadeIn() },
-                    exitTransition = { slideOutHorizontally(targetOffsetX = { it }) + fadeOut() }
-                ) { backStackEntry ->
+                composable("esercizio/{giornoId}/{gruppoMuscolareId}/{esercizioId}") { backStackEntry ->
                     val giornoId = backStackEntry.arguments?.getString("giornoId") ?: return@composable
                     val gruppoMuscolareId = backStackEntry.arguments?.getString("gruppoMuscolareId") ?: return@composable
                     val esercizioId = backStackEntry.arguments?.getString("esercizioId") ?: return@composable
@@ -120,7 +95,6 @@ fun ContentScreen(navController: NavHostController) {
                     )
                 }
             }
-
         }
     )
 }
